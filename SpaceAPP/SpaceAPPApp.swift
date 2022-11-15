ea420ee9e6e7c4d73f0f4d4885af30dacf629f76
Issa-morad <112824795+Issa-morad@users.noreply.github.com>
2022-11-15 10:43:53 +0100

    Initial Commit

---
+++ SpaceAPP/SpaceAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceAPPApp.swift
+//  SpaceAPP
+//
+//  Created by Issa Morad on 2022-11-15.
+//
+
+import SwiftUI
+
+@main
+struct SpaceAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
