34ca926d67b0157f650a273f0cf808897fd65ca3
Issa-morad <112824795+Issa-morad@users.noreply.github.com>
2022-12-02 07:59:00 +0100

    Changed some things

--- SpaceAPP/view models/IssManager.swift
+++ SpaceAPP/view models/IssManager.swift
@@ -12,16 +12,6 @@ import Foundation
 class IssManager {
     let url = URL(string: "https://api.wheretheiss.at/v1/satellites/25544")!
     
-    func getString() async -> String {
-        do {
-            let request = URLRequest(url: url)
-            let (data,error) = try await URLSession.shared.data(for: request)
-            return String(data: data, encoding: .utf8) ?? error.debugDescription
-        }
-        catch {
-            return error.localizedDescription
-        }
-    }
     func getIss() async  -> Iss? {
         do {
             let request = URLRequest (url: url)
